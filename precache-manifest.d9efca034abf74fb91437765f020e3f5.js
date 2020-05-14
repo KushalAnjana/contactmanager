e3a820dc92411751a1df62b5d504276b5689b313
kushal <kushal@04.com>
2020-05-15 00:10:10 +0530

    Updates

---
+++ precache-manifest.d9efca034abf74fb91437765f020e3f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8a5ca020b15997e9b4cd7435c22f10",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "d4c6ad71272973daf8f0",
+    "url": "/contactmanager/static/css/2.019da84b.chunk.css"
+  },
+  {
+    "revision": "958262043d483cc3c0f3",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d4c6ad71272973daf8f0",
+    "url": "/contactmanager/static/js/2.62a2b6fc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.62a2b6fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "958262043d483cc3c0f3",
+    "url": "/contactmanager/static/js/main.bbc1295c.chunk.js"
+  },
+  {
+    "revision": "3959f31844f85f026a86",
+    "url": "/contactmanager/static/js/runtime-main.711199b3.js"
+  }
+]);
